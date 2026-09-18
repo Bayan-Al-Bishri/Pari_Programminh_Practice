cab095dc74e233394fe6b1329894fec6aad6160a
u011t <danaabdulgader@gmail.com>
2026-09-18 22:16:42 +0300

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -1,1 +1,8 @@
-public class main 
+public class Question5{
+  public static void main(String[] args){
+    System.out.println(2*(5/2 + 5/2));//8
+    System.out.println(2*5 / 2+2 * 5/2);//10
+    System.out.println(2*(5/2));//4
+    System.out.println(2*5 / 2);//5
+  }
+}
