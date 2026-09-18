7e4b3adb23fbc5c254939474e263cac2b73aa1ae
mnijoud <adtrle45@icloud.com>
2026-09-18 16:34:28 +0300

    Create Question2.java

---
+++ Question2.java
@@ -0,0 +1,10 @@
+public class Question2 {
+    public static void main(String[] args) {
+
+        int i = 2;
+        int j = 2;
+        int k = 2;
+
+        System.out.println(i + " " + j + " " + k);
+    }
+}
